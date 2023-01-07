bc324f95d28b6675c9ea8682b1b224237c032358
anildev26 <anilsahudev26@gmail.com>
2023-01-07 23:47:25 +0530

    Day 7 Code

---
+++ gsNumberOfPeopleAwareOfASecret.java
@@ -0,0 +1,21 @@
+package challenge_6Company_30Days;
+
+public class gsNumberOfPeopleAwareOfASecret {
+
+    public int peopleAwareOfSecret(int n, int delay, int forget) {
+        long dp[] = new long[n + 1], mod = (long)1e9 + 7, share = 0, res = 0;
+        dp[1] = 1;
+        for (int i = 2; i <= n; ++i)
+            dp[i] = share = (share + dp[Math.max(i - delay, 0)] - dp[Math.max(i - forget, 0)] + mod) % mod;
+        for (int i = n - forget + 1; i <= n; ++i)
+            res = (res + dp[i]) % mod;
+        return (int)res;
+    }
+
+    public static void main(String[] args) {
+        int n=6;
+        int delay=2;
+        int forget=4;
+
+    }
+}
